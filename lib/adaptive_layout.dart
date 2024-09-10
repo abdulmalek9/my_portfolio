894c636cb64bdb1648ba4ca3c14575d93ab474d3
abdulmalek9 <akabdalmalek@gmail.com>
2024-09-10 10:20:14 +0300

    first commit "init app"

---
+++ lib/adaptive_layout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/utils/size_config.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout(
+      {super.key,
+      required this.mobileWidget,
+      required this.tabletWidget,
+      required this.desktopWidgt});
+
+  final WidgetBuilder mobileWidget, tabletWidget, desktopWidgt;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraines) {
+        if (constraines.maxWidth > SizeConfig.desktopPoint) {
+          return desktopWidgt(context);
+        } else if (constraines.maxWidth > SizeConfig.tabletPoint) {
+          return tabletWidget(context);
+        } else {
+          return mobileWidget(context);
+        }
+      },
+    );
+  }
+}
---
+++ lib/utils/app_images.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesChatApp
+  /// assets/images/chat_app.png
+  static const String imagesChatApp = "assets/images/chat_app.png";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.png
+  static const String imagesDashboard = "assets/images/dashboard.png";
+
+  /// Assets for imagesNewsApp
+  /// assets/images/news_app.png
+  static const String imagesNewsApp = "assets/images/news_app.png";
+
+  /// Assets for imagesNotesApp
+  /// assets/images/notes_app.png
+  static const String imagesNotesApp = "assets/images/notes_app.png";
+
+  /// Assets for imagesWaetherApp
+  /// assets/images/waether_app.png
+  static const String imagesWaetherApp = "assets/images/waether_app.png";
+
+  /// Assets for imagesWaneesApp
+  /// assets/images/wanees_app.png
+  static const String imagesWaneesApp = "assets/images/wanees_app.png";
+
+  /// Assets for imagesWaneesAppSvg
+  /// assets/images/wanees_app.svg.svg
+  static const String imagesWaneesAppSvg = "assets/images/wanees_app.svg.svg";
+}
+
